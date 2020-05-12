c43f3a781c8e0f0434a9cb9d6835f58254416733
Tim Seymore <tim_seymore2012@hotmail.com>
2020-05-11 20:37:19 -0400

    Update test.py

--- test.py
+++ test.py
@@ -10,8 +10,12 @@ class TestCase(unittest.TestCase):
         self.assertEqual(make_date("1234-01-02").get_month(), Date(1234, 1, 2).get_month())
         self.assertEqual(make_date("1234-01-02").get_year(), Date(1234, 1, 2).get_year())
 
-
-
+    def test_make_time(self):
+        self.assertEqual(type(make_time('12:30:59,123')), type(Time(12, 30, 59, 123)))
+        self.assertEqual(make_time('12:30:59,123').get_hr(), Time(12, 30, 59, 123).get_hr())
+        self.assertEqual(make_time('12:30:59,123').get_mins(), Time(12, 30, 59, 123).get_mins())
+        self.assertEqual(make_time('12:30:59,123').get_sec(), Time(12, 30, 59, 123).get_sec())
+        self.assertEqual(make_time('12:30:59,123').get_ms(), Time(12, 30, 59, 123).get_ms())
 
 
 
